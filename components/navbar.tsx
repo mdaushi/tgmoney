bc8f599c4cc68451b52347ee8d69ca0e3180bf3a
mdaushi <firdaus.ex19@gmail.com>
2023-11-28 22:56:23 +0800

    add header component

---
+++ components/navbar.tsx
@@ -0,0 +1,19 @@
+import { MainNav } from '@/components/mainNav';
+import { Input } from '@/components/ui/input';
+
+export default function Navbar() {
+  return (
+    <div className='border-b'>
+      <div className='flex h-16 items-center px-4'>
+        <MainNav className='mx-6' />
+        <div className='ml-auto flex items-center space-x-4'>
+          <Input
+            type='search'
+            placeholder='Search...'
+            className='md:w-[100px] lg:w-[300px]'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
